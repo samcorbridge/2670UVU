4206d0b3b438cd1c0086c3f92d0dea06ff1279af
Unknown <10595387@my.uvu.edu>
2017-08-23 15:54:17 -0600

    triggerclasswork

---
+++ FallSemester/Assets/triggerscript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerscript : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter() {
+        print("Entered");
+	}
+	
+	// Update is called once per frame
+	void OnTriggerExit() {
+        print("Exited");
+	}
+}
